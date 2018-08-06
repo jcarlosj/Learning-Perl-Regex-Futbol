2f1d65b81719f78ca3452a75e5d1a45465754e5d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-08-06 09:59:30 -0500

    Perl: Crea expresión regular para listar todos los partidos jugados en enero

--- perl_regex.pl
+++ perl_regex.pl
@@ -3,13 +3,26 @@
 use strict;
 use warnings;
 
+my $match = 0;
+my $nomatch = 0;
+
 # Abre el archivo
 open( my $file, "<./files/results.csv" ) or die( "No hay archivo" );
 
 # Itera cada uno de las líneas del archivo
 while( <$file> ) {
-    print $_;
+    chomp;      # Quita los saltos de línea y los caracteres raros
+    # Usamos la función m (match) para validar nuestra expresión que en Perl se encierra entre / slashes
+    if( m/^[\d]{4,4}\-02-.*$/ ) {
+        print $_ ."\n";
+        $match++;
+    }
+    else {
+        $nomatch++;
+    }
 }
 
 # Cierra el archivo
 close( $file );
+
+printf( "Se encontraron %d aciertos \n", $match );
